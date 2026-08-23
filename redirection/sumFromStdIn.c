f456c5e414db76ef0d4a13d0c0f1e7abdefd8339
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-08-23 18:55:39 -0400

    added examples

---
+++ redirection/sumFromStdIn.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+  // TODO: read numbers from stdin, sum them
+  // until EOF is read, print the sum
+  return 0;
+}
---
+++ static/count.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void foo(){
+  // TODO: Print the number of times this function has been called
+}
+
+int main(int argc, char *argv[]){
+  // take a number as a command line argument and
+  // call foo() that many times
+  return 0;
+}
