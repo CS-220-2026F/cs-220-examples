4d98adbf6cfb61ae634e97f2727cde2d2eed6d82
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-08-29 14:42:23 -0400

    updated example

--- simple_arith/arithTest.c
+++ simple_arith/arithTest.c
@@ -2,14 +2,27 @@
 #include <stdlib.h>
 #include "arith.h"
 
+int a;
+int b = 1;
+
 int main(int argc, char *argv[]){
-  /* TODO: Get the two integers passed in through the command line
+  /* Get the two integers passed in through the command line
      and perform each of the operations defined in arith.c,
      printing the result in the form "1 + 2 = 3".
      We will need to use the "atoi" function to convert the
      command line arguments from ascii to integers, and
      the "printf" function to print our result.
   */
-  
+  if (argc > 1)
+    a = atoi(argv[1]);
+  if (argc > 2)
+    b = atoi(argv[2]);
+
+  printf("%d + %d = %d\n", a, b, add(a,b));
+  printf("%d - %d = %d\n", a, b, sub(a,b));
+  printf("%d * %d = %d\n", a, b, mul(a,b));
+  printf("%d / %d = %d\n", a, b, divide(a,b));
+  printf("%d %% %d = %d\n", a, b, remain(a,b));
+  printf("%d ** %d = %d\n", a, b, expnt(a,b));
   return 0;
 }
