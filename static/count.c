991d4ef43bf83803f0bde49c9a8a143100c9b98e
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-09-01 20:25:19 -0400

    updated example

--- static/count.c
+++ static/count.c
@@ -3,10 +3,17 @@
 
 void foo(){
   // TODO: Print the number of times this function has been called
+  static int count = 0;
+  count++;
+  printf("function has been called %d times\n", count);
 }
 
 int main(int argc, char *argv[]){
   // take a number as a command line argument and
   // call foo() that many times
+  int n = atoi(argv[1]);
+  for (int i = 0; i < n; i++){
+    foo();
+  }
   return 0;
 }
