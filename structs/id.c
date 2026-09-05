4e6096fec0123198fb345c7863b9be1f8df4920a
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-09-05 12:44:17 -0400

    updated example

--- structs/id.c
+++ structs/id.c
@@ -9,16 +9,28 @@ struct person {
 
 void printPerson(struct person p){
   // TODO: print each field in the struct
+  printf("name = %s\n", p.name);
+  printf("age = %d\n", p.age);
 }
 
 // TODO: write a constructor
 struct person *createPerson(char *name, int age){
-
-  return NULL; // change this
+  struct person *p = malloc(sizeof(struct person));
+  strcpy(p->name, name);
+  p->age = age;
+  return p; 
 }
 
 int main(){
   // TODO: initialize three person structs in three different ways
-  
+  struct person p1 = {"Alice", 51};
+  printPerson(p1);
+  struct person p2;
+  strcpy(p2.name,"Bob");
+  p2.age = 27;
+  printPerson(p2);
+  struct person *p3 = createPerson("Charles", 38);
+  printPerson(*p3);
+  free(p3);
   return 0;
 }
