e95598aceeced0de9958b685dc17392eacf3270e
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-09-05 12:46:26 -0400

    updated example

--- variable/var.c
+++ variable/var.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main(){
-
+  int num = 100;
+  printf("value of num = %d, address of num = %p\n", num, &num);
+  int *ptr = &num; // "int *" means pointer to an int
+  // & is the reference operator
+  printf("value of ptr = %p, address of ptr = %p\n", ptr, &ptr);
+  printf("ptr points to %d\n", *ptr); // * is the dereference operator
+  *ptr = 200;
+  printf("value of num = %d, address of num = %p\n", num, &num);
   return 0;
   
 }
